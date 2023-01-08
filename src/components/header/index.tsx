d91c1f6011a91a5e0045a76e55a198a098399b6a
Marcos Ferreira <marcosferreira.developer@gmail.com>
2023-01-08 10:00:10 -0300

    create global menu with button showMenu

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,33 +1,43 @@
+import { useState } from "react";
 import { LogoHeader } from "../logo";
 import { Menu } from "../menu";
 
 import './styles.css';
 
 export function Header() {
+
+  const [menuShow, setMenuShow] = useState<boolean>(false);
+
+  function handleMenuShow() {
+    setMenuShow(menuShow ? false : true);
+  }
+
   return (
     <header className="header menubar">
-      <div className="container">
-        <div className="header-container">
-          <div className="header-content">
-            <button className="btn btn-menu" >
-              <i id="button-menu-icon" className="fa fa-bars" aria-hidden="true"></i>
-            </button>
-            <LogoHeader />
-          </div>
-          <div className="header-content">
-            <ul>
-              <li className="mobile-hidden"><a href="http://esic.pmdonaines.pb.gov.br/" target="_blank">Acesso à Informação</a></li>
-              <li className="mobile-hidden"><a href="https://pmdonaines.pb.gov.br/carta-de-servicos/" target="_blank">Carta de Serviços</a></li>
-              <li className="mobile-hidden"><a className="btn btn-emphasis" href="https://transparencia.pmdonaines.pb.gov.br" target="_blank">Portal da Transparência</a></li>
-            </ul>
+      <div className="menu-shadow">
+        <div className="container">
+          <div className="header-container">
+            <div className="header-content">
+              <button className="btn btn-menu" onClick={() => handleMenuShow()}>
+                <i id="button-menu-icon" className="fa fa-bars" aria-hidden="true"></i>
+              </button>
+              <LogoHeader />
+            </div>
+            <div className="header-content">
+              <ul>
+                <li className="mobile-hidden"><a href="http://esic.pmdonaines.pb.gov.br/" target="_blank">Acesso à Informação</a></li>
+                <li className="mobile-hidden"><a href="https://pmdonaines.pb.gov.br/carta-de-servicos/" target="_blank">Carta de Serviços</a></li>
+                <li className="mobile-hidden"><a className="btn btn-emphasis" href="https://transparencia.pmdonaines.pb.gov.br" target="_blank">Portal da Transparência</a></li>
+              </ul>
 
-            <a className="btn" href="https://pmdonaines.pb.gov.br/?s" target="_blank">
-              <span className="material-icons-outlined">search</span>
-            </a>
+              <a className="btn" href="https://pmdonaines.pb.gov.br/?s" target="_blank">
+                <span className="material-icons-outlined">search</span>
+              </a>
+            </div>
           </div>
-        </div>
-      </div >
-      <Menu />
+        </div >
+      </div>
+      <Menu show={menuShow} />
     </header >
   );
 }
--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,56 +1,50 @@
 import './styles.css';
 
-export function Menu() {
-  return (
-    <div className="menu-global">
-      <div className="container">
-        <div>
+type PropsType = {
+  show: boolean
+}
+
+export function Menu({ show }: PropsType) {
+  return show ? (
+    <div className="menu">
+      <div className="menu-container">
+        <section>
+          <h4>Acesso à Informação</h4>
           <ul>
-            <li>
-              <h4>Acesso à Informação</h4>
-            </li>
             <li><a href="https://transparencia.pmdonaines.pb.gov.br/" target="_blank">Portal da Transparência</a></li>
             <li><a href="https://esic.pmdonaines.pb.gov.br/" target="_blank">e-SIC - Informação ao Cidadão</a></li>
             <li><a href="https://pmdonaines.pb.gov.br/carta-de-servicos/" target="_blank">Carta de Serviços</a></li>
             <li><a href="https://pmdonaines.pb.gov.br/category/diario-oficial/" target="_blank">Diário Oficial do Município</a></li>
           </ul>
-        </div>
-        <div>
+        </section>
+        <section>
+          <h4>Coronavírus</h4>
           <ul>
-            <li>
-              <h4>Coronavírus</h4>
-            </li>
             <li><a href="https://pmdonaines.pb.gov.br/covid-19/" target="_blank">Transparência Covid-19</a></li>
             <li><a href="https://pmdonaines.pb.gov.br/relatorios-covid-19/" target="_blank">Relatórios Covid-19</a></li>
           </ul>
-        </div>
-        <div>
+        </section>
+        <section>
+          <h4>Município</h4>
           <ul>
-            <li>
-              <h4>Município</h4>
-            </li>
             <li><a href="https://pmdonaines.pb.gov.br/historia-do-municipio/" target="_blank">História do Município</a></li>
             <li><a href="https://pmdonaines.pb.gov.br/hino/" target="_blank">Hino do Município</a></li>
             <li><a href="https://pmdonaines.pb.gov.br/feriados-municipais/" target="_blank">Feriados Municipais</a></li>
             <li><a href="https://pmdonaines.pb.gov.br/links-uteis/" target="_blank">Links Úteis</a></li>
           </ul>
-        </div>
-        <div>
+        </section>
+        <section>
+          <h4>Governo</h4>
           <ul>
-            <li>
-              <h4>Governo</h4>
-            </li>
             <li><a href="https://pmdonaines.pb.gov.br/a-prefeitura/" target="_blank">Prefeitura</a></li>
             <li><a href="https://pmdonaines.pb.gov.br/o-prefeito/" target="_blank">Prefeito</a></li>
             <li><a href="https://pmdonaines.pb.gov.br/o-vice-prefeito/" target="_blank">Vice-prefeito</a></li>
             <li><a href="#" target="_blank">Secretários e Diretores</a></li>
           </ul>
-        </div>
-        <div>
+        </section>
+        <section>
+          <h4>Secretarias e Órgãos</h4>
           <ul>
-            <li>
-              <h4>Secretarias e Orgãos</h4>
-            </li>
             <li><a href="https://pmdonaines.pb.gov.br/secretarias-e-orgaos/gabinete-do-prefeito/" target="_blank">Gabinete do Prefeito</a></li>
             <li><a href="https://pmdonaines.pb.gov.br/secretarias-e-orgaos/secretaria-municipal-de-administracao-e-financas/" target="_blank">Administração e Finanças</a></li>
             <li><a href="https://pmdonaines.pb.gov.br/wp-content/uploads/2021/10/CLIQUE-AQUI-PARA-ACESSAR-O-EDITAL-2.png" target="_blank">Assistência Social e Habitação</a></li>
@@ -60,32 +54,26 @@ export function Menu() {
             <li><a href="https://pmdonaines.pb.gov.br/secretarias-e-orgaos/secretaria-municipal-de-saude/" target="_blank">Saúde</a></li>
             <li><a href="#">Impresp</a></li>
           </ul>
-        </div>
-        <div>
+        </section>
+        <section>
+          <h4>Departamentos</h4>
           <ul>
-            <li>
-              <h4>Departamentos</h4>
-            </li>
             <li><a href="#">Agricultura</a></li>
             <li><a href="#">Obras</a></li>
             <li><a href="#">Limpeza Pública</a></li>
             <li><a href="#">Tecnologia da Informação</a></li>
           </ul>
-        </div>
-        <div>
+        </section>
+        <section>
+          <h4>Setores</h4>
           <ul>
-            <li>
-              <h4>Setores</h4>
-            </li>
             <li><a href="#">Sala do Empreendedor</a></li>
             <li><a href="#">Tecnologia</a></li>
           </ul>
-        </div>
-        <div>
+        </section>
+        <section>
+          <h4>Legislação</h4>
           <ul>
-            <li>
-              <h4>Legislação</h4>
-            </li>
             <li><a href="https://pmdonaines.pb.gov.br/category/leis" target="_blank">Leis Municipais</a></li>
             <li><a href="https://pmdonaines.pb.gov.br/leis-municipais/" target="_blank">Leis na Câmera Municipal</a></li>
             <li><a href="https://pmdonaines.pb.gov.br/lei-organica-do-municipio-de-dona-ines-pb/" target="_blank">Lei Orgânica do Município</a></li>
@@ -96,9 +84,8 @@ export function Menu() {
             <li><a href="https://pmdonaines.pb.gov.br/category/loa/" target="_blank">LOA</a></li>
             <li><a href="https://pmdonaines.pb.gov.br/category/ppa/" target="_blank">PPA</a></li>
           </ul>
-        </div>
+        </section>
       </div>
-
     </div>
-  );
+  ) : <span></span>;
 }
