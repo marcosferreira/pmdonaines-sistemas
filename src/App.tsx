b693cdd2d305ca9a0c57f8e63a13e95db95a5a81
Marcos Ferreira <marcosferreira.developer@gmail.com>
2023-01-10 16:43:50 -0300

    fix layout footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Home } from "./pages/home"
 import { Footer } from './components/footer';
 
 import './App.css';
-import { LogoHeader } from "./components/logo";
 import { Header } from "./components/header";
 import { TransparencyBar } from "./components/transparency-bar";
 
