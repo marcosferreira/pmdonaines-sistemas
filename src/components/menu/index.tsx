a3d047179a9e9fb42cf8ff9ae8744d1c5f7c3f20
Marcos Ferreira <marcosferreira.developer@gmail.com>
2023-02-10 16:20:32 -0300

    fix static urls menu

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,4 +1,5 @@
 import { Dispatch, SetStateAction } from 'react';
+
 import './styles.css';
 
 type PropsType = {
@@ -21,14 +22,14 @@ export function Menu({ menuShow, setMenuShow }: PropsType) {
             <li><a href="https://transparencia.pmdonaines.pb.gov.br/" target="_blank">Portal da Transparência</a></li>
             <li><a href="https://esic.pmdonaines.pb.gov.br/" target="_blank">e-SIC - Informação ao Cidadão</a></li>
             <li><a href="https://pmdonaines.pb.gov.br/carta-de-servicos/" target="_blank">Carta de Serviços</a></li>
-            <li><a href="https://pmdonaines.pb.gov.br/category/diario-oficial/" target="_blank">Diário Oficial do Município</a></li>
+            <li><a href="https://transparencia.pmdonaines.pb.gov.br/category/diario-oficial/" target="_blank">Diário Oficial do Município</a></li>
           </ul>
         </section>
         <section>
           <h4>Coronavírus</h4>
           <ul>
-            <li><a href="https://pmdonaines.pb.gov.br/covid-19/" target="_blank">Transparência Covid-19</a></li>
-            <li><a href="https://pmdonaines.pb.gov.br/relatorios-covid-19/" target="_blank">Relatórios Covid-19</a></li>
+            <li><a href="https://transparencia.pmdonaines.pb.gov.br/covid-19/" target="_blank">Transparência Covid-19</a></li>
+            <li><a href="https://transparencia.pmdonaines.pb.gov.br/covid-19/relatorios-covid-19/" target="_blank">Relatórios Covid-19</a></li>
           </ul>
         </section>
         <section>
@@ -54,7 +55,7 @@ export function Menu({ menuShow, setMenuShow }: PropsType) {
           <ul>
             <li><a href="https://pmdonaines.pb.gov.br/secretarias-e-orgaos/gabinete-do-prefeito/" target="_blank">Gabinete do Prefeito</a></li>
             <li><a href="https://pmdonaines.pb.gov.br/secretarias-e-orgaos/secretaria-municipal-de-administracao-e-financas/" target="_blank">Administração e Finanças</a></li>
-            <li><a href="https://pmdonaines.pb.gov.br/wp-content/uploads/2021/10/CLIQUE-AQUI-PARA-ACESSAR-O-EDITAL-2.png" target="_blank">Assistência Social e Habitação</a></li>
+            <li><a href="https://pmdonaines.pb.gov.br/secretarias-e-orgaos/secretaria-municipal-de-assistencia-social-e-habitacao/" target="_blank">Assistência Social e Habitação</a></li>
             <li><a href="https://pmdonaines.pb.gov.br/secretarias-e-orgaos/secretaria-municipal-de-cultura-e-turismo/" target="_blank">Cultura e Turismo</a></li>
             <li><a href="https://pmdonaines.pb.gov.br/secretarias-e-orgaos/secretaria-municipal-de-educacao-e-desporto/" target="_blank">Educação e Desporto</a></li>
             <li><a href="https://pmdonaines.pb.gov.br/secretarias-e-orgaos/secretaria-de-infraestrutura-agricultura-e-meio-ambiente/" target="_blank">Infraestrutura e Meio Ambiente</a></li>
@@ -81,18 +82,18 @@ export function Menu({ menuShow, setMenuShow }: PropsType) {
         <section>
           <h4>Legislação</h4>
           <ul>
-            <li><a href="https://pmdonaines.pb.gov.br/category/leis" target="_blank">Leis Municipais</a></li>
-            <li><a href="https://pmdonaines.pb.gov.br/leis-municipais/" target="_blank">Leis na Câmera Municipal</a></li>
-            <li><a href="https://pmdonaines.pb.gov.br/lei-organica-do-municipio-de-dona-ines-pb/" target="_blank">Lei Orgânica do Município</a></li>
-            <li><a href="https://pmdonaines.pb.gov.br/category/decretos/" target="_blank">Decretos</a></li>
-            <li><a href="https://pmdonaines.pb.gov.br/lc-01-codigo-tributario/" target="_blank">Código Tributário</a></li>
+            <li><a href="https://transparencia.pmdonaines.pb.gov.br/category/leis/" target="_blank">Leis Municipais</a></li>
+            <li><a href="https://transparencia.pmdonaines.pb.gov.br/decretos/" target="_blank">Decretos Municipais</a></li>
+            <li><a href="https://transparencia.pmdonaines.pb.gov.br/leis/lei-organica-do-municipio-de-dona-ines-pb/" target="_blank">Lei Orgânica do Município</a></li>
             <li><a href="http://www.acessoainformacao.gov.br/assuntos/conheca-seu-direito/a-lei-de-acesso-a-informacao" target="_blank">Regulamento da LAI</a></li>
-            <li><a href="https://pmdonaines.pb.gov.br/category/ldo/" target="_blank">LDO</a></li>
-            <li><a href="https://pmdonaines.pb.gov.br/category/loa/" target="_blank">LOA</a></li>
-            <li><a href="https://pmdonaines.pb.gov.br/category/ppa/" target="_blank">PPA</a></li>
+            <li><a href="https://transparencia.pmdonaines.pb.gov.br/planejamento-e-orcamento/ldo/" target="_blank">LDO</a></li>
+            <li><a href="https://transparencia.pmdonaines.pb.gov.br/planejamento-e-orcamento/loa-lei-orcamentaria-anual/" target="_blank">LOA</a></li>
+            <li><a href="https://transparencia.pmdonaines.pb.gov.br/planejamento-e-orcamento/ppa-plano-plurianual/" target="_blank">PPA</a></li>
+            <li><a href="https://pmdonaines.pb.gov.br/lc-01-codigo-tributario/" target="_blank">Código Tributário</a></li>
           </ul>
         </section>
       </div>
     </div>
-  ) : <span></span>;
+
+ ) : <span></span>;
 }
